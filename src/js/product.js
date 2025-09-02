3f5fbc0a294ddab8d8c21214218007bba6600975
Christian Uche <christianuche720@gmail.com>
2025-09-02 10:17:36 -0700

    fixed the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  //setLocalStorage("so-cart", product); This is incorrect, it overwrites the cart
+  let cartItems = getLocalStorage("so-cart") || []; // get existing cart items or initialize as empty array or get cart array from local storage if null set to empty array
+  if (!Array.isArray(cartItems)) {
+    console.error("Cart data is corrupted. Resetting cart.");
+    cartItems = []; // reset to empty array if corrupted
+  }
+
+  cartItems.push(product); // add new product to cart array
+  setLocalStorage("so-cart", cartItems); // save updated cart array back to local storage
+  alert(`${product.Name} has been added to your cart!`); // notify user what has been added to the Cart
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +24,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById("addToCart") // assuming button has id of addToCart
+  .addEventListener("click", addToCartHandler); // attach event listener to button
